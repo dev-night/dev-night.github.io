410fc839b126f5917ab31b09d016085706b2e3d6
Niklas Heer <nih@tradebyte.biz>
2016-10-05 17:42:10 +0200

    added automatic links

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-$('#dark_mode').on('click', function(e){
-	$('body').toggleClass('solarized-dark');
+$(document).ready(function() {
+	$('#dark_mode').on('click', function(e){
+		$('body').toggleClass('solarized-dark');
 
-	// Make button display current state
-	$('#dark_mode').toggleClass('btn-default');
-	$('#dark_mode').toggleClass('btn-success');
-})
+		// Make button display current state
+		$('#dark_mode').toggleClass('btn-default');
+		$('#dark_mode').toggleClass('btn-success');
+
+		$('.linkjuice__icon').toggleClass('linkjuice__icon-white');
+	})
+
+	linkjuice.init('.container',{selectors:['h2','h3','h4','h5'],icon:'<i class="linkjuice__icon"></i>'});
+});
