5035fe3033324091ef717056ab246e416d7473f8
Niklas Heer <niklas.heer@gmail.com>
2018-11-24 10:51:10 +0100

    Fix night time mode

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 
 	function switchColorMode() {
 		$('body').toggleClass('solarized-dark');
@@ -19,43 +19,43 @@ jQuery(document).ready(function($){
 	}
 
 	// Dark Mode Button
-	$('#dark_mode').on('click', function(e){
+	$('#dark_mode').on('click', function (e) {
 		switchColorMode();
 	})
 
 	// Switch color mode based on the time of the day
 	const hours = new Date().getHours()
-	const isNightTime = hours => 19 && hours < 6
-	if (isNightTime) {
+	if (hours >= 19 || hours <= 6) {
+		console.log("true")
 		switchColorMode();
 	}
 
-	linkjuice.init('.container',{selectors:['h2','h3','h4','h5'],icon:'<i class="linkjuice__icon"></i>'});
+	linkjuice.init('.container', { selectors: ['h2', 'h3', 'h4', 'h5'], icon: '<i class="linkjuice__icon"></i>' });
 
 	// Scroll To Top
 	// browser window scroll (in pixels) after which the "back to top" link is shown
 	var offset = 50,
-	//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-	offset_opacity = 1200,
-	//duration of the top scrolling animation (in ms)
-	scroll_top_duration = 700,
-	//grab the "back to top" link
-	$back_to_top = $('.cd-top');
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//grab the "back to top" link
+		$back_to_top = $('.cd-top');
 
 	//hide or show the "back to top" link
-	$(window).scroll(function(){
-		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-		if( $(this).scrollTop() > offset_opacity ) {
+	$(window).scroll(function () {
+		($(this).scrollTop() > offset) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if ($(this).scrollTop() > offset_opacity) {
 			$back_to_top.addClass('cd-fade-out');
 		}
 	});
 
 	//smooth scroll to top
-	$back_to_top.on('click', function(event){
+	$back_to_top.on('click', function (event) {
 		event.preventDefault();
 		$('body,html').animate({
-			scrollTop: 0 ,
-		 	}, scroll_top_duration
+			scrollTop: 0,
+		}, scroll_top_duration
 		);
 	});
 
@@ -63,7 +63,7 @@ jQuery(document).ready(function($){
 	var current = new Date();
 	var start = new Date("2018-12-10 10:00:00")
 
-	if(start.getTime()<=current.getTime()){
+	if (start.getTime() <= current.getTime()) {
 		$('.livestream-section').append(" \
 			<iframe id='ls_embed_1539691978' src='https://livestream.com/accounts/23776270/events/8406262/player?width=640&height=360&enableInfoAndActivity=true&defaultDrawer=&autoPlay=true&mute=false' width='816' height='440' frameborder='0' scrolling='no' allowfullscreen> </iframe> \
 			<script type='text/javascript' data-embed_id='ls_embed_1539691978' src='https://livestream.com/assets/plugins/referrer_tracking.js'></script> \
